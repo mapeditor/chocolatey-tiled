7b3cbc8739fac5d848bb384a10c7afd830d0d2eb
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2016-01-09 14:12:29 +0100

    Tiled 0.15.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$url = "https://github.com/bjorn/tiled/releases/download/v0.14.1/tiled-0.14.1-win32-setup.exe"
-$url64 = "https://github.com/bjorn/tiled/releases/download/v0.14.1/tiled-0.14.1-win64-setup.exe"
+$url = "https://github.com/bjorn/tiled/releases/download/v0.15.0/tiled-0.15.0-win32-setup.exe"
+$url64 = "https://github.com/bjorn/tiled/releases/download/v0.15.0/tiled-0.15.0-win64-setup.exe"
 
 Install-ChocolateyPackage "tiled" "exe" "/S" "$url" "$url64"

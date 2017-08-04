71f57ac5ff4a76610ffddb2c07c899f98f779ba6
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2017-08-04 12:41:19 +0200

    Tiled 1.0.2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$url = "https://github.com/bjorn/tiled/releases/download/v0.16.2/tiled-0.16.2-win32.msi"
-$url64 = "https://github.com/bjorn/tiled/releases/download/v0.16.2/tiled-0.16.2-win64.msi"
+$url = "https://github.com/bjorn/tiled/releases/download/v1.0.2/Tiled-1.0.2-win32.msi"
+$url64 = "https://github.com/bjorn/tiled/releases/download/v1.0.2/Tiled-1.0.2-win64.msi"
 
 Install-ChocolateyPackage "tiled" "msi" "/quiet" "$url" "$url64" -validExitCodes @(0,3010)

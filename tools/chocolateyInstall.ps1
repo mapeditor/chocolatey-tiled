fc0b4c85e0cbe944bc28972c6a82c574f8235442
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2020-03-09 21:50:32 +0100

    add checksum for 1.3.3

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 $url64 = "https://github.com/bjorn/tiled/releases/download/v1.3.3/Tiled-1.3.3-win64.msi"
-$checksum = "4e8c3851dbab4c91ea8fc304f8d49256601ab561a124c8620c64bc23c922321e"
+$checksum = "C8F36277AEB0F881C6A6AD6132F14FA535061BFED594B1CF522553B61562BCDC"
 
 Install-ChocolateyPackage -PackageName "tiled" -FileType "msi" -SilentArgs "/quiet" -Url64bit "$url64" -ChecksumType "sha256" -Checksum $checksum -validExitCodes @(0,3010)

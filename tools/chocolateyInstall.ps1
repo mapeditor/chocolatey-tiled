affeaef78a348660ac07e63c08549e22ff04e782
Andrew <kerr.jnr@xtra.co.nz>
2020-06-22 11:35:00 +1200

    Upgraded to v1.4.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$url64 = "https://github.com/bjorn/tiled/releases/download/v1.3.5/Tiled-1.3.5-win64.msi"
-$checksum = "F60FDCA64B54CE22697CD6D254DE6FD89112486DD8FBE24F4DBFB0C529E11AF1"
+$url64 = "https://github.com/bjorn/tiled/releases/download/v1.4.0/Tiled-1.4.0-win64.msi"
+$checksum = "3FBAEAB20258EC953FC9097A70655F8FA77CFEBBC32B9A33F8555AD524765210"
 
 Install-ChocolateyPackage -PackageName "tiled" -FileType "msi" -SilentArgs "/quiet" -Url64bit "$url64" -ChecksumType "sha256" -Checksum $checksum -validExitCodes @(0,3010)

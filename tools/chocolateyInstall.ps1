d53f1e68a023ad003976494fbf613266916c7a4b
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2022-02-12 17:43:06 +0100

    Tiled 1.8.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$url64 = "https://github.com/mapeditor/tiled/releases/download/v1.8.0/Tiled-1.8.0-win64.msi"
-$checksum = "E3541C58D9D197E0A910017FD4E55133E873E5EE11B5D217EDD8CD130576F54E"
+$url64 = "https://github.com/mapeditor/tiled/releases/download/v1.8.1/Tiled-1.8.1-win64.msi"
+$checksum = "FA55982F155DBA3DD48E7186C9CD4AFFDBCAEE663201B9C1A85B565D6BAA8697"
 
 Install-ChocolateyPackage -PackageName "tiled" -FileType "msi" -SilentArgs "/quiet" -Url64bit "$url64" -ChecksumType "sha256" -Checksum $checksum -validExitCodes @(0,3010)

aab439ce0df59a01cd16cfb632f0f43a5d9cfd39
Andrew <kerr.jnr@xtra.co.nz>
2022-04-03 11:36:08 +1200

    Upgraded to v1.8.4.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$url64 = "https://github.com/mapeditor/tiled/releases/download/v1.8.2/Tiled-1.8.2-win64.msi"
-$checksum = "84FF99E851541FFBBCFC2D6A574156E3D52390C28BA8D42975DE3F177A14DBE5"
+$url64 = "https://github.com/mapeditor/tiled/releases/download/v1.8.4/Tiled-1.8.4-win64.msi"
+$checksum = "7F534A4FC149E27AE2747918D01643BCEE3F5C9AEC4F241EB0BD40463321C547"
 
 Install-ChocolateyPackage -PackageName "tiled" -FileType "msi" -SilentArgs "/quiet" -Url64bit "$url64" -ChecksumType "sha256" -Checksum $checksum -validExitCodes @(0,3010)

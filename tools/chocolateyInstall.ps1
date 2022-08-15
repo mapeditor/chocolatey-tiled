715ab59d59ad89b4396b5c1b7d07a83e769c18bd
Andrew <kerr.jnr@xtra.co.nz>
2022-08-15 12:49:29 +1200

    Upgraded to v1.9.1.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,5 @@
-$url64 = "https://github.com/mapeditor/tiled/releases/download/v1.9.0/Tiled-1.9.0_Windows_x86_64.msi"
-$checksum = "D8D578C90481CDB4209BF3D5E584B95633D83BFA1437D1A52BDCCAE9B41CE102"
+$url64 = "https://github.com/mapeditor/tiled/releases/download/v1.9.1/Tiled-1.9.1_Windows_x86_64.msi"
+$checksum = "F3E68CA4A4CDC415F374EFEC9652318BB6038ADB82868AC5DB47C2FAAEAED761"
+
 
 Install-ChocolateyPackage -PackageName "tiled" -FileType "msi" -SilentArgs "/quiet" -Url64bit "$url64" -ChecksumType "sha256" -Checksum $checksum -validExitCodes @(0,3010)

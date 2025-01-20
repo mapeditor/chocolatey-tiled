b94101c0745c398fce0782ef9e2e85f433346614
t460s-win <kerr.jnr@outlook.co.nz>
2025-01-20 14:40:56 +1300

    Upgraded to v1.11.1.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-$url32 = "https://github.com/mapeditor/tiled/releases/download/v1.11.0/Tiled-1.11.0_Windows-7-8_x86.msi"
-$checksum32 = "FB7A0CA755F43CB9934EABC5462CE8D095DA4DA143F72DBC2D7CA0EAC35B59A2"
-$url64 = "https://github.com/mapeditor/tiled/releases/download/v1.11.0/Tiled-1.11.0_Windows-10+_x86_64.msi"
-$checksum64 = "A727E599741BAAC5032B3C96E24E397C1AD045CFCD24CBD0B126CAC66A61B93E"
+$url32 = "https://github.com/mapeditor/tiled/releases/download/v1.11.1/Tiled-1.11.1_Windows-7-8_x86.msi"
+$checksum32 = "E2084D2611145DF699272F1A93E6F312FFF5114952BE4D775C18E1FA744391A4Get"
+$url64 = "https://github.com/mapeditor/tiled/releases/download/v1.11.1/Tiled-1.11.1_Windows-10+_x86_64.msi"
+$checksum64 = "9E397581C25A201B0F8121B3DFB9D7B3E3878FECA0FF22C1345E2E0F98E64A1E"
 
 Install-ChocolateyPackage -PackageName "tiled" -FileType "msi" -SilentArgs "/quiet" -Url "$url32" -Url64bit "$url64" -ChecksumType "sha256" -Checksum $checksum32 -Checksum64 $checksum64 -validExitCodes @(0,3010)
